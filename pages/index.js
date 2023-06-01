2d0d56b626bcac05c9c926c3eb883de4411704cd
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-31 21:38:42 -0300

    NextAuth with Google: initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+
 export default function Home() {
+
+  const { data: session } = useSession();
+
+  // Si el usuario no está logeado
+  if (!session) {
+    return (
+      <div className="bg-blue-800 w-screen h-screen flex items-center">
+        <div className="w-full text-center">
+          <button 
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  // Si el usuario está logeado
   return (
-    <div className='bg-slate-500'>
-      <h1>Test</h1>
+    <div>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
     </div>
   )
 }
